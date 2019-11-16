34e573edb89179a980fb845a82b8e467781b3394
antoniolopez1 <antoniolopez0063@gmail.com>
2019-11-16 12:57:06 -0300

    typescript

---
+++ typescript/app.js
@@ -0,0 +1,21 @@
+var animal1 = "perro";
+var animal2 = 'gato';
+var animal3 = "raton";
+var concat = "Animales: " + animal1 + ", " + animal2 + ", " + animal3;
+//console.log(concat)
+var Persona = /** @class */ (function () {
+    function Persona() {
+    }
+    return Persona;
+}());
+var persona1 = new Persona();
+var n1;
+var n2 = 1;
+if (n1 != undefined || n2 != undefined) {
+    if (n2 < n1) {
+        console.log("Soy toby");
+    }
+    else {
+        console.log("Hola a todos");
+    }
+}
---
+++ typescript/app.ts
@@ -0,0 +1,25 @@
+let animal1:string = "perro";
+let animal2:string = 'gato';
+let animal3:string = `raton`;
+
+
+
+let concat:string= `Animales: ${animal1}, ${animal2}, ${animal3}`
+
+//console.log(concat)
+class Persona{
+
+  constructor();
+}
+let persona1= new Persona();
+
+let n1:number;
+let n2:number = 1;
+if(n1!=undefined || n2!=undefined){
+  if(n2<n1){
+    console.log("Soy toby");
+  }
+  else{
+    console.log("Hola a todos");
+  }
+}
